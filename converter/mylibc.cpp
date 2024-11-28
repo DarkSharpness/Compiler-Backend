e9d555a06707bf4868f7ffaa4dd53909ee4c172e
Wankupi <2893353848@qq.com>
2024-11-28 22:24:10 +0800

    fix(libc): add calloc

--- converter/mylibc.cpp
+++ converter/mylibc.cpp
@@ -1,6 +1,7 @@
 
 #include <cstddef>
-constexpr auto MALLOC_SIZE = 1024 * 1024 * 256;
+#include <cstring>
+constexpr auto MALLOC_SIZE = 1024 * 1024 * 512;
 unsigned char malloc_buf[MALLOC_SIZE];
 unsigned char *malloc_ptr = malloc_buf;
 
@@ -11,4 +12,11 @@ extern "C" void *_my_malloc(size_t size) {
 	unsigned char *ret = malloc_ptr;
 	malloc_ptr += (size + 15) & ~15;
 	return ret;
+}
+extern "C" void *_my_calloc(size_t nitems, size_t size) {
+	void *ptr = _my_malloc(nitems * size);
+	if (ptr) {
+		memset(ptr, 0, nitems * size);
+	}
+	return ptr;
 }
